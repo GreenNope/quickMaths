50032d7039a5941a46fb03190a8fe67b3c8ebcce
GreenNope <tlash1000@gmail.com>
2017-12-22 14:25:40 -0500

    Update quadratic.js

--- quadratic.js
+++ quadratic.js
@@ -1,8 +1,14 @@
 function getFactors(n) {
+    factorArray = []
     n % 2 == 0 ? interval = 1 : interval = 2;
-    for (i = 1; i <= n^0.5; i++) 
+    for (i = 1; i <= n^0.5; i = i + interval) {
+        if (n % i == 0) {
+            factorArray.push([i, n/i])
+            
+        }
     
     
     
     
     }
+}
